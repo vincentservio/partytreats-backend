cab5493d0c5ec3a3e0bbca9d6d185c3ca97830d5
vincentservio <vincentservio@gmail.com>
2021-01-27 17:24:49 -0500

    heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-    resources :customers
-
-  resources :pastries
-
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+resources :pastries
+resources :customers
 end
